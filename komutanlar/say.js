b118ede2aa011f7e7cfabee03a03b4ddcefeacde
Turkiyem-mod <85872618+Turkiyem-mod@users.noreply.github.com>
2021-07-16 18:04:46 +0300

    Create say.js

---
+++ komutanlar/say.js
@@ -0,0 +1,10 @@
+module.exports = {
+  name: "say",
+  code: `$color[RANDOM] $description[
+Toplam Kişi $replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$membersCount;1;:one:;-1];2;:two:;-1];3;:three:;-1];4;:four:;-1];5;:five:;-1];6;:six:;-1];7;:seven:;-1];8;:eight:;-1];9;:nine:;-1];0;:zero:;-1] 
+Çevrimiçi Kişi $replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$membersCount[$guildID;online];1;:one:;-1];2;:two:;-1];3;:three:;-1];4;:four:;-1];5;:five:;-1];6;:six:;-1];7;:seven:;-1];8;:eight:;-1];9;:nine:;-1];0;:zero:;-1] 
+Rahatsız Etmeyindeki Kişi $replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$membersCount[$guildID;dnd];1;:one:;-1];2;:two:;-1];3;:three:;-1];4;:four:;-1];5;:five:;-1];6;:six:;-1];7;:seven:;-1];8;:eight:;-1];9;:nine:;-1];0;:zero:;-1] Boşta Kişi $replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$membersCount[$guildID;idle];1;:one:;-1];2;:two:;-1];3;:three:;-1];4;:four:;-1];5;:five:;-1];6;:six:;-1];7;:seven:;-1];8;:eight:;-1];9;:nine:;-1];0;:zero:;-1] 
+Görünmezdeki Kişi $replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$membersCount[$guildID;offline];1;:one:;-1];2;:two:;-1];3;:three:;-1];4;:four:;-1];5;:five:;-1];6;:six:;-1];7;:seven:;-1];8;:eight:;-1];9;:nine:;-1];0;:zero:;-1] 
+Bot Sayısı $replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$botCount;1;:one:;-1];2;:two:;-1];3;:three:;-1];4;:four:;-1];5;:five:;-1];6;:six:;-1];7;:seven:;-1];8;:eight:;-1];9;:nine:;-1];0;:zero:;-1]]
+`
+}
