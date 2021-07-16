80155a95b071e12ac490086510f78fe6f7a03a79
Turkiyem-mod <85872618+Turkiyem-mod@users.noreply.github.com>
2021-07-16 07:59:40 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-
+const DBD = require("dbd.js")
+var fs = require('fs');
+const keep_alive = require("./keep_alive.js")
+const bot = new DBD.Bot({
+  token: process.env['token'],
+  prefix: "H!",
+  mobile: false,
+  fetchInvites: true
+})
+bot.onMessage()
+var reader = fs.readdirSync("./komutlar").filter(file => file.endsWith(".js"))
+for (const file of reader) {
+  const command = require(`./komutlar/${file}`)
+  bot.command({
+    name: command.name,
+    aliases: command.aliases,
+    bkz: command.bkz,
+    code: command.code
+  });
+}
